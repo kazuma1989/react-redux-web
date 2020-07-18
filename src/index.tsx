fc831e11ee9eca8c1ae8afa5fc7268006dd7dec7
kazuma1989 <kazuma.ebina@quipper.com>
2020-07-18 18:30:06 +0900

    スタイリングなしで HTML 構造だけ作る

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function App() {
-  return <h1>Hello React!</h1>
+  return (
+    <div>
+      <header>
+        <div>Kanban board</div>
+
+        <input placeholder="Filter cards" />
+      </header>
+
+      <div>
+        <section>
+          <h3>TODO</h3>
+
+          <article>朝食をとる🍞</article>
+          <article>SNSをチェックする🐦</article>
+          <article>布団に入る (:3[___]</article>
+        </section>
+
+        <section>
+          <h3>Doing</h3>
+
+          <article>顔を洗う👐</article>
+          <article>歯を磨く🦷</article>
+        </section>
+
+        <section>
+          <h3>Waiting</h3>
+        </section>
+
+        <section>
+          <h3>Done</h3>
+
+          <article>布団から出る (:3っ)っ -=三[＿＿]</article>
+        </section>
+      </div>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
