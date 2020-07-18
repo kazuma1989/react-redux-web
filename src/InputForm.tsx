2f2c64654c269cd20c7c6b3520a017bd42ca7ae0
kazuma1989 <kazuma.ebina@quipper.com>
2020-07-18 21:09:12 +0900

    自動調節機能をカスタムフックにする

--- src/InputForm.tsx
+++ src/InputForm.tsx
@@ -22,20 +22,7 @@ export function InputForm({
     onConfirm?.()
   }
 
-  const ref = useRef<HTMLTextAreaElement>(null)
-
-  useEffect(
-    () => {
-      const el = ref.current
-      if (!el) return
-
-      const { borderTopWidth, borderBottomWidth } = getComputedStyle(el)
-      el.style.height = 'auto' // 一度 auto にしないと高さが縮まなくなる
-      el.style.height = `calc(${borderTopWidth} + ${el.scrollHeight}px + ${borderBottomWidth})`
-    },
-    // 内容が変わるたびに高さを再計算
-    [value],
-  )
+  const ref = useAutoFitToContentHeight(value)
 
   return (
     <Container className={className}>
@@ -57,6 +44,30 @@ export function InputForm({
       </ButtonRow>
     </Container>
   )
+}
+
+/**
+ * テキストエリアの高さを内容に合わせて自動調整する
+ *
+ * @param content テキストエリアの内容
+ */
+function useAutoFitToContentHeight(content: string | undefined) {
+  const ref = useRef<HTMLTextAreaElement>(null)
+
+  useEffect(
+    () => {
+      const el = ref.current
+      if (!el) return
+
+      const { borderTopWidth, borderBottomWidth } = getComputedStyle(el)
+      el.style.height = 'auto' // 一度 auto にしないと高さが縮まなくなる
+      el.style.height = `calc(${borderTopWidth} + ${el.scrollHeight}px + ${borderBottomWidth})`
+    },
+    // 内容が変わるたびに高さを再計算
+    [content],
+  )
+
+  return ref
 }
 
 const Container = styled.div``
