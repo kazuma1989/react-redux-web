0f57a6e34a784bd6313e7f86ca4034fc98f3d57c
kazuma1989 <kazuma.ebina@quipper.com>
2020-07-18 18:19:03 +0900

    Prettier を実行する

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return <h1>Hello React!</h1>
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'))
