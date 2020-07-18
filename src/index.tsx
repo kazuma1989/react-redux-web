2a4355d87bf076b49906fd96e32fe73374104f53
kazuma1989 <kazuma.ebina@quipper.com>
2020-07-18 18:34:26 +0900

    styled-components で DOM の実体を抽象化する

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div>
-      <header>
-        <div>Kanban board</div>
+    <Container>
+      <Header>
+        <Logo>Kanban board</Logo>
 
-        <input placeholder="Filter cards" />
-      </header>
+        <CardFilter placeholder="Filter cards" />
+      </Header>
 
-      <div>
-        <section>
-          <h3>TODO</h3>
+      <MainArea>
+        <Column>
+          <ColumnHeader>TODO</ColumnHeader>
 
-          <article>朝食をとる🍞</article>
-          <article>SNSをチェックする🐦</article>
-          <article>布団に入る (:3[___]</article>
-        </section>
+          <Card>朝食をとる🍞</Card>
+          <Card>SNSをチェックする🐦</Card>
+          <Card>布団に入る (:3[___]</Card>
+        </Column>
 
-        <section>
-          <h3>Doing</h3>
+        <Column>
+          <ColumnHeader>Doing</ColumnHeader>
 
-          <article>顔を洗う👐</article>
-          <article>歯を磨く🦷</article>
-        </section>
+          <Card>顔を洗う👐</Card>
+          <Card>歯を磨く🦷</Card>
+        </Column>
 
-        <section>
-          <h3>Waiting</h3>
-        </section>
+        <Column>
+          <ColumnHeader>Waiting</ColumnHeader>
+        </Column>
 
-        <section>
-          <h3>Done</h3>
+        <Column>
+          <ColumnHeader>Done</ColumnHeader>
 
-          <article>布団から出る (:3っ)っ -=三[＿＿]</article>
-        </section>
-      </div>
-    </div>
+          <Card>布団から出る (:3っ)っ -=三[＿＿]</Card>
+        </Column>
+      </MainArea>
+    </Container>
   )
 }
+
+const Container = styled.div``
+
+const Header = styled.div``
+
+const Logo = styled.div``
+
+const CardFilter = styled.input``
+
+const MainArea = styled.div``
+
+const Column = styled.div``
+
+const ColumnHeader = styled.div``
+
+const Card = styled.div``
 
 ReactDOM.render(<App />, document.getElementById('app'))
