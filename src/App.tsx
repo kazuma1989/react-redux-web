db0bdf9585a67a87560561ae2b22ad33c55facbf
kazuma1989 <kazuma.ebina@quipper.com>
2020-07-18 23:06:54 +0900

    API 通信を実装してデータを永続化する

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import produce from 'immer'
 import { randomID } from './util'
+import { api } from './api'
 import { Header as _Header } from './Header'
 import { Column } from './Column'
 import { DeleteDialog } from './DeleteDialog'
@@ -97,6 +98,10 @@ export function App() {
   }
 
   const addCard = (columnID: string) => {
+    const column = columns.find(c => c.id === columnID)
+    if (!column) return
+
+    const text = column.text
     const cardID = randomID()
 
     type Columns = typeof columns
@@ -112,6 +117,11 @@ export function App() {
         column.text = ''
       }),
     )
+
+    api('POST /v1/cards', {
+      id: cardID,
+      text,
+    })
   }
 
   const [deletingCardID, setDeletingCardID] = useState<string | undefined>(
