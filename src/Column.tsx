61e9797af6ce648391d48057fafb71ba9f48bd07
kazuma1989 <kazuma.ebina@quipper.com>
2020-07-19 10:37:55 +0900

    Column が常にレンダリングされるのを改善する

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { useSelector } from 'react-redux'
+import { useSelector, shallowEqual } from 'react-redux'
 import * as color from './color'
 import { Card } from './Card'
 import { PlusIcon } from './icon'
@@ -8,19 +8,28 @@ import { InputForm as _InputForm } from './InputForm'
 import { ColumnID } from './api'
 
 export function Column({ id: columnID }: { id: ColumnID }) {
-  const { column, cards, filtered, totalCount } = useSelector(state => {
-    const filterValue = state.filterValue.trim()
-    const filtered = Boolean(filterValue)
-    const keywords = filterValue.toLowerCase().split(/\s+/g)
+  const { column, cards, filtered, totalCount } = useSelector(
+    state => {
+      const filterValue = state.filterValue.trim()
+      const filtered = Boolean(filterValue)
+      const keywords = filterValue.toLowerCase().split(/\s+/g)
 
-    const column = state.columns?.find(c => c.id === columnID)
-    const cards = column?.cards?.filter(({ text }) =>
-      keywords.every(w => text?.toLowerCase().includes(w)),
-    )
-    const totalCount = column?.cards?.length ?? -1
+      const { title, cards: rawCards } =
+        state.columns?.find(c => c.id === columnID) ?? {}
 
-    return { column, cards, filtered, totalCount }
-  })
+      const column = { title }
+      const cards = rawCards
+        ?.filter(({ text }) =>
+          keywords.every(w => text?.toLowerCase().includes(w)),
+        )
+        .map(c => c.id)
+      const totalCount = rawCards?.length ?? -1
+
+      return { column, cards, filtered, totalCount }
+    },
+    (left, right) =>
+      Object.keys(left).every(key => shallowEqual(left[key], right[key])),
+  )
   const draggingCardID = useSelector(state => state.draggingCardID)
 
   const [inputMode, setInputMode] = useState(false)
@@ -50,13 +59,13 @@ export function Column({ id: columnID }: { id: ColumnID }) {
           {filtered && <ResultCount>{cards.length} results</ResultCount>}
 
           <VerticalScroll>
-            {cards.map(({ id }, i) => (
+            {cards.map((id, i) => (
               <Card.DropArea
                 key={id}
                 targetID={id}
                 disabled={
                   draggingCardID !== undefined &&
-                  (id === draggingCardID || cards[i - 1]?.id === draggingCardID)
+                  (id === draggingCardID || cards[i - 1] === draggingCardID)
                 }
               >
                 <Card id={id} />
@@ -68,7 +77,7 @@ export function Column({ id: columnID }: { id: ColumnID }) {
               style={{ height: '100%' }}
               disabled={
                 draggingCardID !== undefined &&
-                cards[cards.length - 1]?.id === draggingCardID
+                cards[cards.length - 1] === draggingCardID
               }
             />
           </VerticalScroll>
